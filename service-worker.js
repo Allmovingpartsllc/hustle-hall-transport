44fbc7c761dfe8567922e9b9872d13ef2ba6c42a
Allmovingpartsllc <griffine.ppca1@gmail.com>
2026-07-29 12:24:16 -0400

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ firebase.messaging().onBackgroundMessage((payload) => {
   });
 });
 
-const CACHE_NAME = 'hustle-hall-v18';
+const CACHE_NAME = 'hustle-hall-v19';
 const APP_FILES = [
   './',
   './index.html',
