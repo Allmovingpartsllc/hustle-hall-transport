628a52ca2542f1e861451fb05d21eed92b100b59
Allmovingpartsllc <griffine.ppca1@gmail.com>
2026-08-31 08:38:14 -0400

    Restrict Driver Dashboard to admins

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ firebase.messaging().onBackgroundMessage((payload) => {
   });
 });
 
-const CACHE_NAME = 'hustle-hall-v35';
+const CACHE_NAME = 'hustle-hall-v36';
 const APP_FILES = [
   './',
   './index.html',
