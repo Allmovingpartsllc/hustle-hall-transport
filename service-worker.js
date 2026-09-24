5151c520841618d350cdd3440a8bb7833e799be4
Allmovingpartsllc <griffine.ppca1@gmail.com>
2026-09-24 17:01:00 -0400

    Refresh offline cache for school multi-day promo

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ firebase.messaging().onBackgroundMessage((payload) => {
   });
 });
 
-const CACHE_NAME = 'hustle-hall-v39';
+const CACHE_NAME = 'hustle-hall-v40';
 const APP_FILES = [
   './',
   './index.html',
