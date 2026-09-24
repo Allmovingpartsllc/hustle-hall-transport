cd0ba3f49307b8ff0d6448a0d2f3beac0b3d567a
Allmovingpartsllc <griffine.ppca1@gmail.com>
2026-09-24 16:31:56 -0400

    Refresh offline cache for multi-day rides

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ firebase.messaging().onBackgroundMessage((payload) => {
   });
 });
 
-const CACHE_NAME = 'hustle-hall-v37';
+const CACHE_NAME = 'hustle-hall-v38';
 const APP_FILES = [
   './',
   './index.html',
