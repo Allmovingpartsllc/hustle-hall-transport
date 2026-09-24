670f3654300b9a04b0b3f13fac0afa6681b2842a
Allmovingpartsllc <griffine.ppca1@gmail.com>
2026-09-24 17:02:43 -0400

    Refresh offline cache after promo cleanup

--- service-worker.js
+++ service-worker.js
@@ -18,7 +18,7 @@ firebase.messaging().onBackgroundMessage((payload) => {
   });
 });
 
-const CACHE_NAME = 'hustle-hall-v40';
+const CACHE_NAME = 'hustle-hall-v41';
 const APP_FILES = [
   './',
   './index.html',
